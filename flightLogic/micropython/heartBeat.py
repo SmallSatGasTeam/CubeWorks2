b1949fd9d775062eec588310fa93518b6bc7ceaf
Taylor Rowser <taylorrowser@outlook.com>
2022-03-01 15:15:23 -0700

    heartbeat complete rewrite in MicroPython

--- flightLogic/micropython/heartBeat.py
+++ flightLogic/micropython/heartBeat.py
@@ -1,19 +1,29 @@
+# pylint: disable=E,W,C,R
+# type: ignore
+
 import uasyncio
-import RPi.GPIO as GPIO
+from machine import Pin
 
 class heart_beat:
+    __heartbeat_pin = 25
+    __pin = None
+
     def __init__(self) -> None:
-        GPIO.setwarnings(False)
-        GPIO.setmode(GPIO.BCM) #Physical Pin numbering
-        GPIO.setup(21, GPIO.OUT, initial=GPIO.LOW) #Sets pin 40 (GPIO 21) to be an output pin and sets the initial value to low (off)
+        print("Test")
+        self.__pin = Pin(self.__heartbeat_pin, Pin.OUT)
 
 
     async def heartBeatRun(self): #Sets up up-and-down voltage on pin 40 (GPIO 21) for heartbeat with Arduino
         waitTime = 4
         while True:
-            GPIO.output(21, GPIO.HIGH)
+            self.__pin.value(1)
             print("Heartbeat wave high")
             await uasyncio.sleep(waitTime/2)
-            GPIO.output(21, GPIO.LOW)
+            
+            self.__pin.value(0)
             print("Heartbeat wave low")
             await uasyncio.sleep(waitTime/2)
+
+# This will be called by the parent, but for testing...
+heartBeat = heart_beat()
+uasyncio.run(heartBeat.heartBeatRun())
